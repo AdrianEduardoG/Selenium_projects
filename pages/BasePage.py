33e29e593c0c2f0875d9610684a424c7c80f0dbc
Adrian <deksoner2112@gmail.com>
2024-07-09 00:20:05 -0600

    click_by_driver was addes to execute click by receiving a webdriver object

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -8,9 +8,15 @@ class BasePage:
     def find_element(self, by, selector):
         return self.driver.find_element(by, selector)
     
+    def find_elements(self, by, selector):
+        return self.driver.find_elements(by, selector)
+    
     def click(self, by, selector):
         element  = self.driver.find_element(by, selector)
         element.click()
+
+    def click_by_driver(self, driver):
+        driver.click()
 
     def type_text(self, by, selector, text):
         element = self.driver.find_element(by, selector)
