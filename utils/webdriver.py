217c257359af2d93070ac77c91b6384376f12c0f
Adrian <deksoner2112@gmail.com>
2024-07-09 17:20:08 -0600

    ChromeOptions was added and commented to debug with browser

--- utils/webdriver.py
+++ utils/webdriver.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
 
 def get_chrome_driver():
+    # options = webdriver.ChromeOptions()
+    # options.add_experimental_option("detach", True)
+    # return webdriver.Chrome(options=options)
     return webdriver.Chrome()
