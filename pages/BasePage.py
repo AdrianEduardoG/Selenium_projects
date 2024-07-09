25c1c091f40d7e590e4e59699cf65d5ec6278dba
Adrian <deksoner2112@gmail.com>
2024-07-09 17:18:12 -0600

    click_by_script, click_selected_buttons, click_all_buttons, select_by_value were created to support InventoryPage

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -16,15 +16,25 @@ class BasePage:
         return WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((by, selector)))
     
     def click(self, by, selector):
-        element  = self.driver.find_element(by, selector)
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(element))
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((by, selector))).click()
+
+    def click_by_script(self, by, selector):
+        element = self.find_element(by, selector)
         self.driver.execute_script("arguments[0].click();", element)
 
-    # def click_by_script(self, by, selector):
-    #     element = self.find_element(by, selector)
-    #     WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(element))
-    #     # self.driver.execute_script("arguments[0].click();", element)
-    #     element.click()
+    def click_selected_buttons(self, by, selector, element_selected):
+        element = self.find_elements(by, selector)
+        element[element_selected].click()
+
+    def click_all_buttons(self, by, selector):
+        elements = self.find_elements(by, selector)
+        for element in elements:
+            element.click()
+
+    def select_by_value(self, by, selector, value):
+        element = self.find_element(by, selector)
+        select = Select(element)
+        select.select_by_value(value)
 
     def type_text(self, by, selector, text):
         element = self.driver.find_element(by, selector)
