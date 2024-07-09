144b3a6acf28f2ce8694dcaa8a69be53620c3688
Adrian <deksoner2112@gmail.com>
2024-07-09 11:48:10 -0600

    restructuration of the methods, oriented to elements and actions

--- pages/InventoryPage.py
+++ pages/InventoryPage.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .BasePage import BasePage
+
 import time
 
 class InventoryPage(BasePage):
@@ -17,20 +18,18 @@ class InventoryPage(BasePage):
         # self.CART_BUTTON_CLASS          = "shopping_cart_link" 
         # self.LEFT_MENU_ID               = "react-burger-menu-btn"
 
+        self.LEFT_MENU          = (By.ID, "react-burger-menu-btn")
+        self.MENU_ABOUT         = (By.ID, "about_sidebar_link")
         self.ADD_TO_CART_BUTTON = (By.XPATH, "//div[@class='inventory_list']//button")
         self.ITEM_TITLE         = By.LINK_TEXT
         self.SELECT_FILTER_BOX  = (By.CLASS_NAME, "product_sort_container")
         self.CART_BUTTON_CLASS  = (By.CLASS_NAME, "shopping_cart_link")
-        self.LEFT_MENU          = (By.ID, "react-burger-menu-btn")
 
-    def add_cart(self):
-        buttons = self.find_elements(self.ADD_TO_CART_BUTTON[0], self.ADD_TO_CART_BUTTON[1])
+    def menu_button(self):
+        self.click(self.LEFT_MENU[0], self.LEFT_MENU[1])
 
-        for button in buttons:
-            self.click_by_driver(button)
+    def menu_about(self):
+        self.menu_button()
+        self.click(self.MENU_ABOUT[0], self.MENU_ABOUT[1])
 
-        return len(buttons)
-    
-    def cart_button_mount(self):
-        return self.find_element(self.CART_BUTTON_CLASS[0], self.CART_BUTTON_CLASS[1])
     
