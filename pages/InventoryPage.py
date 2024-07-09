2f759a65fd5c8ef51f4e4f6121c7c7ef96f05706
Adrian <deksoner2112@gmail.com>
2024-07-09 17:19:08 -0600

    Restructuration of the Inventory Page, based on interactions and methots.

--- pages/InventoryPage.py
+++ pages/InventoryPage.py
@@ -20,16 +20,45 @@ class InventoryPage(BasePage):
 
         self.LEFT_MENU          = (By.ID, "react-burger-menu-btn")
         self.MENU_ABOUT         = (By.ID, "about_sidebar_link")
+        self.MENU_LOGOUT        = (By.ID, "logout_sidebar_link")
+        self.MENU_CLOSE         = (By.ID, "react-burger-cross-btn")    
+        # self.ITEM_TITLE         = (By.XPATH, "//div[@class='inventory_item_label']//a")
+        self.ELEMENT_TITLE      = By.LINK_TEXT
         self.ADD_TO_CART_BUTTON = (By.XPATH, "//div[@class='inventory_list']//button")
-        self.ITEM_TITLE         = By.LINK_TEXT
         self.SELECT_FILTER_BOX  = (By.CLASS_NAME, "product_sort_container")
         self.CART_BUTTON_CLASS  = (By.CLASS_NAME, "shopping_cart_link")
 
     def menu_button(self):
-        self.click(self.LEFT_MENU[0], self.LEFT_MENU[1])
+        self.click_by_script(self.LEFT_MENU[0], self.LEFT_MENU[1])
 
     def menu_about(self):
         self.menu_button()
-        self.click(self.MENU_ABOUT[0], self.MENU_ABOUT[1])
+        self.click_by_script(self.MENU_ABOUT[0], self.MENU_ABOUT[1])
+
+    def menu_log_out(self):
+        self.menu_button()
+        self.click_by_script(self.MENU_LOGOUT[0],self.MENU_LOGOUT[1])
+
+    def menu_close(self):
+        self.menu_button()
+        self.click_by_script(self.MENU_CLOSE[0],self.MENU_CLOSE[1])
+
+    def element_titles(self, title):
+        self.click(self.ITEM_TITLE, title)
+
+    def add_to_cart(self, element_selected):
+        self.click_selected_buttons(self.ADD_TO_CART_BUTTON[0], self.ADD_TO_CART_BUTTON[1], element_selected)
+
+    def add_all_to_cart(self):
+        self.click_all_buttons(self.ADD_TO_CART_BUTTON[0], self.ADD_TO_CART_BUTTON[1])
+
+    def filter(self, value):
+        self.select_by_value(self.SELECT_FILTER_BOX[0], self.SELECT_FILTER_BOX[1], value)
+
+    def cart(self):
+        self.click(self.CART_BUTTON_CLASS[0], self.CART_BUTTON_CLASS[1])
+    
+    
+
 
     
