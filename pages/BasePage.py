57a6a82e7e9bd808f2e7b22af7e1ecf2dc528e94
Adrian <deksoner2112@gmail.com>
2024-07-10 15:19:42 -0600

    scrollIntoView() was added

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -15,6 +15,10 @@ class BasePage:
     def find_elements(self, by, selector):
         return WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((by, selector)))
     
+    def scroll_into_view(self, by, selector):
+        element = self.find_element(by, selector)
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+    
     def click(self, by, selector):
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((by, selector))).click()
 
